b21d4fff6b5455832bcc416664c2b8a836314e2d
mkolodji <kolodjimichael@gmail.com>
2019-08-29 15:45:28 -0300

    Final módulo 15

--- src/main/java/com/algaworks/brewer/repository/Estilos.java
+++ src/main/java/com/algaworks/brewer/repository/Estilos.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.brewer.model.Estilo;
+import com.algaworks.brewer.repository.helper.estilo.EstiloQueries;
 
 @Repository
-public interface Estilos extends JpaRepository<Estilo, Long> {
+public interface Estilos extends JpaRepository<Estilo, Long>, EstiloQueries {
 
 	public Optional<Estilo> findByNomeIgnoreCase(String nome);
 	
